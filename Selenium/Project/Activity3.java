0800386e3e602e4460d885d97b0021d53f73b2fb
dedwalchitra <128017386+dedwalchitra@users.noreply.github.com>
2023-03-18 00:14:37 +0530

    Add files via upload

---
+++ Selenium/Project/Activity3.java
@@ -0,0 +1,33 @@
+package Project_Activity;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+
+public class Activity3 {
+    WebDriver driver;
+    @Test
+    public void VerifyFirstInfoBox() {
+        WebElement FirstInfoBox=driver.findElement(By.xpath("(//div//h3[@class='uagb-ifb-title'])[1]"));
+        Assert.assertEquals(FirstInfoBox.getText(), "Actionable Training");
+    }
+    @BeforeClass
+    public void beforeClass() {
+        WebDriverManager.firefoxdriver().setup();
+        System.out.println("Launching Firefox browser..");
+        driver = new FirefoxDriver();
+        driver.get("https://alchemy.hguy.co/lms");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.close();
+    }
+
+}
